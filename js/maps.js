0ff35ce1c03b7fef8c0202b15a4661453300e974
Alex <Aleksey.fgl@gmail.com>
2021-06-04 13:41:49 +0300

    fix

--- js/maps.js
+++ js/maps.js
@@ -6,38 +6,23 @@ ymaps.ready(function () {
             searchControlProvider: 'yandex#search'
         }),
 
-        // Создаём макет содержимого.
-        // что это????
         MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
             '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
         ),
 
         myPlacemarkWithContent = new ymaps.Placemark([54.000812, 27.580929], {
-             // баллун
+
             balloonContentHeader:'<img class="map-pic" src="./img/icon/icon-logo.png"> <span class="map-red"> Atlantida </span>',
             balloonContentBody: 'Южная улица, 11',
-            // balloonContentFooter: 'место не очень:)',
-            // hintContent: ' лучше здесь не работать)',
-            // iconContent: '12'
         }, {
-            // Опции.
-            // Необходимо указать данный тип макета.
             iconLayout: 'default#imageWithContent',
-            // Своё изображение иконки метки.
             iconImageHref: './img/index/maps/img-map.png',
-            // Размеры метки.
             iconImageSize: [120, 120],
-            // Смещение левого верхнего угла иконки относительно
-            // её "ножки" (точки привязки).
             iconImageOffset: [-30, -90],
-            // Смещение слоя с содержимым относительно слоя с картинкой.
             iconContentOffset: [0, 0],
-            // Макет содержимого.
             iconContentLayout: MyIconContentLayout
         });
 
-        // Отключение скролла мышкой
         myMap.behaviors.disable(['scrollZoom']);
-        // 
         myMap.geoObjects.add(myPlacemarkWithContent);
 });
